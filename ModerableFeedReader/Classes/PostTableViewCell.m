e075489cc83ab13e7c2a59c9d8415751b4a5db3f
Sergey Klimov <dcdarv@gmail.com>
2012-01-16 21:45:38 -0600

    posts more pretty

--- ModerableFeedReader/Classes/PostTableViewCell.m
+++ ModerableFeedReader/Classes/PostTableViewCell.m
@@ -23,5 +23,16 @@
     if (post.author.imageURL)
         [self.authorPicture setImageWithURL:[NSURL URLWithString:post.author.imageURL]];
 }
+-(void) layoutSubviews {
+    CGSize maximumSize = CGSizeMake(self.title.frame.size.width, self.frame.size.height-self.title.frame.origin.y-3);
+    CGSize stringSize = [self.title.text sizeWithFont:self.title.font 
+                                   constrainedToSize:maximumSize 
+                                       lineBreakMode:self.title.lineBreakMode];
+   
+    CGRect dateFrame = CGRectMake(self.title.frame.origin.x, self.title.frame.origin.y, self.title.frame.size.width, stringSize.height);
+
+    self.title.frame = dateFrame;
+    [super layoutSubviews];
+}
 
 @end
